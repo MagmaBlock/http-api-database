57c2e8f3d9cdb9cd0001421d1000d88d08d1d630
岩浆块Magma <magmablock@qq.com>
2022-09-09 14:03:16 +0800

    扩大转义字符范围为全体4字节Unicode；优化相连的4字节Unicode将会被一并转义；

--- controllers/tools/emojiParser.js
+++ controllers/tools/emojiParser.js
@@ -1,18 +1,18 @@
-import emojiRegex from 'emoji-regex';
+import _ from 'lodash'
 
 export default {
   stringify,
   parse
 }
 
-
 function stringify(text) { // 转义
 
   if (typeof text !== 'string') text = JSON.stringify(text)
 
-  let emojis = text.match(emojiRegex())
-  emojis = new Set(emojis) // 去重
-  emojis = Array.from(emojis)
+  let emojis = text.match(/[\u{10000}-\u{10FFFF}]*/gu);
+
+  emojis = _.compact(emojis)
+  emojis = _.uniq(emojis)
 
   for (let i in emojis) {
     let thisEncodeEmoji = `[emoji:${encodeURIComponent(emojis[i])}]`
@@ -27,8 +27,7 @@ function parse(text) { // 解析
   if (typeof text !== 'string') text = JSON.stringify(text)
 
   let emojis = text.match(/\[emoji\:.*?\]/g)
-  emojis = new Set(emojis)
-  emojis = Array.from(emojis)
+  emojis = _.uniq(emojis)
 
   for (let i in emojis) {
     let emoji = emojis[i].replaceAll('[emoji:', '')
