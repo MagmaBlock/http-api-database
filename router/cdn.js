bcb858f84374f9e50a5bf29518fb05e12c895512
岩浆块Magma <magmablock@qq.com>
2023-05-14 21:34:51 +0800

    移除 CDN 回源相关 API

--- router/cdn.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { cdnGetImage } from "../controllers/cdn/api.js";
-
-const router = Router();
-
-router.get("/getimage/*", cdnGetImage);
-
-export default router;
